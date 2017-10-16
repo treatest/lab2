b3637ff21b699b9c044cb78ff006374cf978863d
treatest <819526936@qq.com>
2017-10-16 17:47:06 +0800

    chenge1

--- lab1/src/lab1/lab1.java
+++ lab1/src/lab1/lab1.java
@@ -3,7 +3,7 @@ package lab1;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileOutpwasdwa
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
--- lab1/src/lab1/lab_1.java
+++ lab1/src/lab1/lab_1.java
@@ -1,5 +1,5 @@
 package lab1;
-
+213
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
